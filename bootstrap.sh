9d03c25ddfc626ca8b751329f5546e88fc8e628a
Collin Buchan <cbuchan@Collins-MacBook-Pro-AngelList.local>
2019-06-26 12:54:08 -0700

    bootstrap improvements

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,8 @@
+# Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm ~/.zshrc
+
+./install
